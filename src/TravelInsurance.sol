7706157dfbfbb1219bbe231a5f9e56798261d9ef
MiK@ <mickael.blondeau@hotmail.fr>
2025-08-27 21:53:19 +0200

    fix: forge build warn

--- src/TravelInsurance.sol
+++ src/TravelInsurance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "./ITravelInsurance.sol";
+import {ITravelInsurance} from "./ITravelInsurance.sol";
 
 contract TravelInsurance is ITravelInsurance {
 
